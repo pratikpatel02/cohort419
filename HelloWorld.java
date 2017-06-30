772d63b132017e138657ec4f7a52d3e86dd40f40
pratikpatel02 <pratikpatel@hotmail.com>
2017-06-30 14:19:03 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ package com.fanniemae;
 //another change
 public class HellowWorld {
 
-	publis static void main(String[] args){
+	public static void main(String[] args){
 
 		System.out.println ("Welcome to Java");
 	}
