1e1d4e5867083a6d1e06f7e77613630a443b7dbe
pratikpatel02 <pratikpatel@hotmail.com>
2017-06-30 14:32:17 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,10 @@ public class HelloWorld {
 	public static void main(String[] args){
 
 		System.out.println ("Welcome to Java");
+		System.out.println ("Update to HelloWorld");
+		
+		for (i=0;i<5;i++){
+			System.out.println ("Update to HelloWorld" + i + "time");
+		}
 	}
 }
